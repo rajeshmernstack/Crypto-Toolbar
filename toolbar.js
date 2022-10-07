9e6574c8609de079e35c98cd38a35cf695cd73ad
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-10-07 15:16:24 +0500

    Added 5mins auto refresh Feature

--- toolbar.js
+++ toolbar.js
@@ -1,31 +1,33 @@
+setInterval(() => {
+  let xhr = new XMLHttpRequest();
 
-let xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=Bitcoin%2CEthereum&vs_currencies=USD&include_24hr_change=true');
 
-xhr.open('GET', 'https://api.coingecko.com/api/v3/simple/price?ids=Bitcoin%2CEthereum&vs_currencies=USD&include_24hr_change=true');
+  xhr.send();
 
-xhr.send();
+  xhr.onload = function () {
+    if (xhr.status != 200) {
+    } else {
+      let data = JSON.parse(xhr.response);
+      document.getElementById('eth-rate').innerText = "" + data.ethereum.usd;
+      document.getElementById('btc-rate').innerText = "" + data.bitcoin.usd;
 
-xhr.onload = function() {
-  if (xhr.status != 200) {
-  } else {
-    let data = JSON.parse(xhr.response);
-    document.getElementById('eth-rate').innerText = ""+data.ethereum.usd;
-    document.getElementById('btc-rate').innerText = ""+data.bitcoin.usd;
+      document.getElementById('rate-change-eth').innerText = "" + data.ethereum.usd_24h_change.toFixed(2);
+      document.getElementById('rate-change-btc').innerText = "" + data.bitcoin.usd_24h_change.toFixed(2);
 
-    document.getElementById('rate-change-eth').innerText = ""+data.ethereum.usd_24h_change.toFixed(2);
-    document.getElementById('rate-change-btc').innerText = ""+data.bitcoin.usd_24h_change.toFixed(2);
-
-    if(data.ethereum.usd_24h_change > 0){
+      if (data.ethereum.usd_24h_change > 0) {
         document.getElementById('rate-change-eth').setAttribute('style', 'color: green');
-    }else{
+      } else {
         document.getElementById('rate-change-eth').setAttribute('style', 'color: red');
-    }
+      }
 
-    if(data.bitcoin.usd_24h_change > 0){
+      if (data.bitcoin.usd_24h_change > 0) {
         document.getElementById('rate-change-btc').setAttribute('style', 'color: green');
-    }else{
+      } else {
         document.getElementById('rate-change-btc').setAttribute('style', 'color: red');
-    }
+      }
 
-  }
-};
+    }
+  };
+
+}, 300000);
